775e0b3f514c7da35c87e505902637124801d908
debugerliu <719918768@qq.com>
2020-04-19 19:49:32 +0800

    新建一个文档

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,16 @@
+from time import sleep
+
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get('http://www.baidu.com')
+driver.get('https://mubu.com/login/password')
+driver.find_element_by_xpath('//*[@id="main-form"]/div[1]/input').send_keys(15035757274)
+driver.find_element_by_xpath('//*[@id="main-form"]/div[2]/input').send_keys(123456)
+driver.find_element_by_xpath('//*[@id="submit"]').click()
+sleep(1)
+driver.find_element_by_xpath('//*[@id="nav"]/div/div[1]').click()
+sleep(1)
+driver.find_element_by_xpath('//*[@id="add-document"]').click()
 print('你看到这个就说明打印成功了')
+sleep(3)
 driver.quit()
